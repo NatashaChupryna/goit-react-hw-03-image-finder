1f782b4fbacddaa3db83399469d8bfbb85c62987
Natasha <natkamonte1992@gmail.com>
2023-01-24 00:39:08 +0200

    .

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,19 +3,15 @@ import { List } from './ImageGallery.styled';
 
 
 
-export const ImageGallery = props => {
-  const { images, error } = props;
+export const ImageGallery = ({images}) => {
   return (
-    <div>
-      {props.error && <h2>{' Please try again'}</h2>}
 
-      {images && (
+   
         <List>
           {images.map(item => (
             <ImageGalleryItem key={item.id} item={item} />
           ))}
         </List>
-      )}
-    </div>
+   
   );
 };
