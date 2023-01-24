9ea5c219880237180fff8c7ceec3aee3b212ecf8
Natasha <natkamonte1992@gmail.com>
2023-01-24 18:55:13 +0200

    fix notification

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,58 +1,58 @@
-
 import { PureComponent } from 'react';
 import { toast } from 'react-hot-toast';
 import {
   Searchbar,
   SearchForm,
   SearchFormButton,
-  SearchFormInput
+  SearchFormInput,
 } from './SearchBar.styled';
 
-
-export class SearchBar extends PureComponent{
+export class SearchBar extends PureComponent {
   state = {
-    searchQuery : ''
-  }
- 
- handleQueryChange = (event) =>{
-this.setState({searchQuery : event.currentTarget.value.toLowerCase()});
-if(this.state.searchQuery.trim() === ''){
-  toast.error('Please, enter your query', {
-    duration: 2000,})
-    return
-  }} 
+    searchQuery: '',
+  };
 
-handleSubmit = (event) => {
-event.preventDefault();
-this.props.onSubmit(this.state.searchQuery);
-event.target.reset()
-// this.setState({searchQuery: ''})
-}
-
-// resetForm = () => {
-//   this.setState({
-//     searchQuery : ''
-//   })
-
-// }
-
-  render(){
-    return (<Searchbar>
-      <SearchForm onSubmit={this.handleSubmit}>
-        <SearchFormButton type="submit" 
-        disabled={this.props.isLoading}>
-          &#128269;
-        </SearchFormButton>
+  handleQueryChange = event => {
+    this.setState({ searchQuery: event.currentTarget.value.toLowerCase() });
   
-        <SearchFormInput
-          type="text"
-          autocomplete="off"
-          placeholder="Search images and photos"
-          onChange={this.handleQueryChange}
-        />
-      </SearchForm>
-    </Searchbar>)
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.searchQuery);
+    if (this.state.searchQuery === '') {
+      toast.error('Please, enter your query', {
+        duration: 2000,
+      });
+      return;
+    }
+
+    // this.setState({searchQuery: ''})
+    event.target.reset();
+  };
+
+  // resetForm = () => {
+  //   this.setState({
+  //     searchQuery : ''
+  //   })
+
+  // }
+  render() {
+    return (
+      <Searchbar>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <SearchFormButton type="submit">
+            &#128269;
+          </SearchFormButton>
+
+          <SearchFormInput
+            type="text"
+            autocomplete="off"
+            placeholder="Search images and photos"
+            onChange={this.handleQueryChange}
+          />
+        </SearchForm>
+      </Searchbar>
+    );
   }
 }
-
-
